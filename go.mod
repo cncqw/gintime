0cd5b22645f4b522f823d4205ee7f8d284d11f7e
cncqw <cncqw@foxmail.com>
2021-01-28 17:41:08 +0800

    create tables

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module alldu.cn/ginproject
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
+	gorm.io/driver/mysql v1.0.4 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.20.12 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,115 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"type:varchar(11);not null;unique"`
+	Password  string `gorm:"size:255;not null"`
+}
 
 func main() {
+	db := InitDB()   //初始化
+	defer db.Close() //延迟关闭
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/api/auth/register", func(c *gin.Context) {
+		//获取参数
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+
+		//数据验证
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号格式不正确"})
+		}
+
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		}
+
+		//如果名称没有传，给一个10位的随机字符串
+		if len(name) == 0 {
+			name = RandomString(10)
+		}
+
+		//判断手机号是否存在
+		if isTelephoneExist(db, telephone) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
+		} else {
+			//创建用户
+			newUser := User{
+				Name:      name,
+				Telephone: telephone,
+				Password:  password,
+			}
+			db.Create(&newUser)
+		}
+
+		//返回结果
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "注册成功",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+}
+
+func RandomString(n int) string {
+	var letters = []byte("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+	result := make([]byte, n)
+
+	rand.Seed(time.Now().Unix())
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	//driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "go_demo"
+	username := "root"
+	password := "root"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database)
+
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database,err:" + err.Error())
+	}
+
+	db.AutoMigrate(&User{})
+
+	return db
+
 }
