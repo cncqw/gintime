5d6f8a18f37ffc34a34084ae186793754136dedc
cncqw <cncqw@foxmail.com>
2021-01-31 01:27:40 +0800

    Add Login

--- controller/UserController.go
+++ controller/UserController.go
@@ -5,6 +5,7 @@ import (
 	"alldu.cn/ginproject/model"
 	"alldu.cn/ginproject/util"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"net/http"
 )
@@ -19,11 +20,13 @@ func Register(c *gin.Context) {
 
 	//数据验证
 	if len(telephone) != 11 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号格式不正确"})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "Incorrect format of mobile phone number"})
+		return
 	}
 
 	if len(password) < 6 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "The password cannot be less than 6 characters."})
+		return
 	}
 
 	//如果名称没有传，给一个10位的随机字符串
@@ -33,21 +36,69 @@ func Register(c *gin.Context) {
 
 	//判断手机号是否存在
 	if isTelephoneExist(DB, telephone) {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已经存在"})
-	} else {
-		//创建用户
-		newUser := model.User{
-			Name:      name,
-			Telephone: telephone,
-			Password:  password,
-		}
-		DB.Create(&newUser)
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "User already exists"})
+		return
 	}
+	//创建用户
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "Encryption error"})
+	}
+	newUser := model.User{
+		Name:      name,
+		Telephone: telephone,
+		Password:  string(hashedPassword),
+	}
+	DB.Create(&newUser)
 
 	//返回结果
 	c.JSON(200, gin.H{
+		"code":    200,
 		"message": "注册成功",
 	})
+}
+
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+
+	//数据验证
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "Incorrect format of mobile phone number"})
+		return
+	}
+
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "The password cannot be less than 6 characters."})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "Mobile does not exist"})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "The password is incorrect"})
+		return
+	}
+
+	//发放token
+	token := "200"
+
+	//返回结果
+	c.JSON(200, gin.H{
+		"code":    200,
+		"data":    gin.H{"token": token},
+		"message": "登录成功",
+	})
+
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
