1b52bc36042ebbd9eb21f099c95b1a1e47f4d43a
cncqw <cncqw@foxmail.com>
2021-02-03 18:59:42 +0800

    Add Vue

---
+++ view/gin-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+     <navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './views/layout/Navbar.vue';
+
+export default {
+  components: { Navbar },
+  data() {
+
+  },
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
